7c4f07041ae5d71851a8afc6891417cb28a94400
Juan (Photonics Desktop) <jevillegasd@gmail.com>
2020-10-26 17:56:29 +0400

    Update on Config Files

--- dependencies/get_TCP_instr.m
+++ dependencies/get_TCP_instr.m
@@ -4,12 +4,25 @@ function [conn ,all_instruments] = get_TCP_instr(instr_type)
     [~,result] = system('arp -a');
     str = splitlines(convertCharsToStrings(result));
     ipaddresses = strings(length(str)-4,1);
+    j=0;
     for i=1:length(ipaddresses)
-        ipaddresses(i) = strtrim(extractBetween(str(i+3),3,17));
+        if ~strcmp(str(i),"")
+           
+            if ~isnan(str2double(extractBetween(str(i),1,3)))
+                j=j+1;
+                ipaddresses(j) = strtrim(extractBetween(str(i),3,17));
+            end
+            
+        end
+        
+    end
+    
+    for i=1:length(str)-j
+        ipaddresses(end)=[];
     end
     clear('s','str','result',"i");
 
-   port=5025; clear data; j = 0;
+    port=5025; clear data; j = 0; data = [];
     for i=1:length(ipaddresses)
         try
             t = tcpclient(ipaddresses(i), port,'Timeout',0.01,'ConnectTimeout',1);
@@ -30,14 +43,19 @@ function [conn ,all_instruments] = get_TCP_instr(instr_type)
             % The IDN is a standard SCPI query for all IEEE compliant
             % measurement systems.
             if(strcmp(Em.identifier, 'MATLAB:networklib:tcpclient:cannotCreateObject'))
-                %disp("Equipment at "+ ipaddresses(i) + " did not allow a connection on port 5025.") 
+                disp("Equipment at "+ ipaddresses(i) + " did not allow a connection on port 5025.") 
             elseif(strcmp(Em.identifier, 'SpectralM:COM:NoResponse'))
-                %disp("Equipment at "+ ipaddresses(i) + " did not respond to IDN query.") 
+                warning("Equipment at "+ ipaddresses(i) + " did not respond to IDN query.") 
             else
                 throw(Em)
             end
             clear Em;
         end
+    end
+    
+    if isempty(data)
+       warning("No instruments were found connected to your network.") 
+       return 
     end
     all_instruments = struct2table(data);
     idx = strcmp(all_instruments.model,instr_type);
